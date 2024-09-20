adda8bb78ce0b8dda0dc715056e99c6d8edacfbb
KyungMin.Ko <rhrudals312@naver.com>
2024-09-20 23:59:23 +0900

    9월20일 챌린지

---
+++ tweets/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Tweet(models.Model):
+    payload = models.CharField(max_length=180)
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.payload
+
+
+class Like(models.Model):
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} likes {self.tweet}"
