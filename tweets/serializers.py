1ed949e7df3b294c45f77f7ccce5f0d21b33c91c
KyungMin.Ko <rhrudals312@naver.com>
2024-09-27 02:31:34 +0900

    9월 27일

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -1,11 +1,23 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Tweet, Like
+from users.models import User
+from rest_framework.serializers import ModelSerializer
 
 
-class TweetSerializer(ModelSerializer):
-    class Meta:
-        model = Tweet
-        fields = "__all__"
+class TweetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    payload = serializers.CharField(max_length=180)
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Tweet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.payload = validated_data.get("payload", instance.payload)
+        instance.updated_at = validated_data.get("updated_at", instance.updated_at)
+        instance.save()
+        return instance
 
 
 class LikeSerializer(ModelSerializer):
--- tweets/urls.py
+++ tweets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import tweet_list
-
+from . import views
 
 urlpatterns = [
-    path("", tweet_list, name="tweet_list"),
+    path("api/v1/tweets", views.tweet_list, name="tweet_list"),
+    path(
+        "api/v1/users/<int:user_id>/tweets",
+        views.user_tweet_list,
+        name="user_tweet_list",
+    ),
 ]
